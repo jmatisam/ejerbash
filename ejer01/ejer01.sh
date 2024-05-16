075dd407d798b4726fb4ad808cc9ac72eb25006f
jmat732 <jmatisamuel@gmail.com>
2024-05-16 12:18:07 +0200

    Agregar carpeta ejer01 con archivos

---
+++ ejer01/ejer01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Definir las variables con los valores deseados
+nombre="Jose Manuel Alonso Tirado"
+edad=50
+ciudad="SEVILLA"
+
+# Crear la cadena de texto usando las variables
+mensaje="Mi nombre es $nombre, tengo $edad años y vivo en $ciudad."
+
+# Imprimir el mensaje
+echo "$mensaje"
